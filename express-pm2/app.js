ed6906fff868ae9844e56a08fd92ba1660aabf57
Santheepkumar <41126042+Santheepkumar@users.noreply.github.com>
2024-11-20 19:37:41 +0530

    Update app.js

--- express-pm2/app.js
+++ express-pm2/app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send(`Hello from PM2 cluster! Instance: ${process.env.NODE_APP_INSTANCE}`);
+    res.send(`Hi from PM2 cluster! Instance: ${process.env.NODE_APP_INSTANCE}`);
+});
+
+app.get('/about', (req, res) => {
+    res.send(`Hello from about page! Instance: ${process.env.NODE_APP_INSTANCE}`);
 });
 
 app.listen(port, () => {
